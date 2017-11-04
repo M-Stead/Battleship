2dc8c7e838fb6f2213cc4e62d40144d923bdd3cd
Matthew Stead <mstead@gmail.com>
2017-11-04 11:38:42 -0600

    Gradle builds and initial project structure commited to allow for building from command line.

---
+++ src/main/java/ca/shaw/bs/game/BattleShip.java
@@ -0,0 +1,10 @@
+package ca.shaw.bs.game;
+
+import ca.shaw.bs.game.Game;
+
+public class BattleShip {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ src/main/java/ca/shaw/bs/game/Game.java
@@ -0,0 +1,13 @@
+package ca.shaw.bs.game;
+
+
+
+public class Game
+{
+
+
+public void start() {
+
+    System.out.printf("Damn, YOU SUNK MY BATTLESHIP");
+}
+}
